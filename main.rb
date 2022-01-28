9d43d0074e1571c1897e35f6c6cf38f49b7605d3
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-28 16:50:54 -0500

    Added rspec and formatting
    Updated the formatting and added rspec for tdd.

--- main.rb
+++ main.rb
@@ -1,10 +1,12 @@
-def caesar_cipher(decrypted, shift)
-    encrypted = decrypted.split('').map do |char|
-        shift.times { |_| char.succ! } if char.match?(/\w/)
-        char[-1]
-    end
+# frozen_string_literal: true
 
-    encrypted.join('')
+def caesar_cipher(decrypted, shift)
+  encrypted = decrypted.split('').map do |char|
+    shift.times { |_| char.succ! } if char.match?(/\w/)
+    char[-1]
+  end
+
+  encrypted.join('')
 end
 
-puts caesar_cipher(ARGV[0] || "Test", ARGV[1].to_i || 2)
+puts caesar_cipher(ARGV[0] || 'Test', ARGV[1].to_i || 2)
